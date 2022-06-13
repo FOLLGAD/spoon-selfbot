448390764e4eff6913db06d724a63314e0454572
Emil Ahlbäck <emil.ahlback99@gmail.com>
2022-06-13 13:34:26 +0200

    added cool timestamps

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 const { Client } = require('discord.js-selfbot-v13');
-const client = new Client(); // All partials are loaded automatically
 const {token} = require("./config.json")
+const stamps = require("./timestamps.json")
 
-client.on('ready', async () => {
-  console.log(`${client.user.username} is ready!`);
-  const user = await client.users.fetch("242000364281266176")
-  user.send("hello :)")
-})
+let lastTime = null
+  console.log("HELLo 2")
 
-client.login(token);
+const fn = () => {
+  console.log("HELLo")
+  const hr = new Date().getHours().toString().padStart(2, "0")
+  const min = new Date().getMinutes().toString().padStart(2, "0");
+  const a = `${hr}:${min}`
+  console.log("checking", a)
+
+  if (a === lastTime) return;
+  lastTime = a;
+
+  const match = stamps[a]
+  
+  if (match) {
+    const msg = match[(Math.random() * match.length)] ?? match[0]
+    console.log("sending", msg)
+
+    const client = new Client(); // All partials are loaded automatically
+    client.on('ready', async () => {
+      // const gdm = await client.channels.fetch("439815556246274078") // the boys
+      const gdm = await client.channels.fetch("985863940414271498") // test
+      await gdm.send(msg)
+      client.destroy()
+    })
+    client.login(token);
+  }
+}
+
+setInterval(fn, 10 * 1000)
+fn()
+
